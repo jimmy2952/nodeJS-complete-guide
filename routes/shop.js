5f6e3235f8c1a02b2510d4a5581fa093018912fa
jimmy2952 <jimmy2952@gmail.com>
2020-10-25 20:30:49 +0800

    templating engines(pug, handlebars, ejs)

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  const products = adminData.products
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0})
 })
 
 module.exports = router
