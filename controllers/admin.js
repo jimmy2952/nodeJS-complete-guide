811f966a53b4135586802744695fd5313d49557d
jimmy2952 <jimmy2952@gmail.com>
2020-10-27 19:30:42 +0800

    dynamic routes and cart implementation

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,30 @@
 const Product = require('../models/product')
 
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit
+  if (!editMode) {
+    return res.redirect('/')
+  }
+  const prodId = req.params.productId
+  Product.findById(prodId, product => {
+    if (!product) {
+      res.redirect('/')
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    })
+  })
+}
+
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product', 
+    path: '/admin/add-product',
+    editing: false
+  })
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -9,9 +32,26 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl
   const price = req.body.price
   const description = req.body.description  
-  const product = new Product(title, imageUrl, price, description)
+  const product = new Product(null, title, imageUrl, price, description)
   product.save()
   res.redirect('/')
+}
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedImageUrl = req.body.imageUrl
+  const updatedPrice = req.body.price
+  const updatedDesc = req.body.description
+  const updatedProduct = new Product(
+    prodId, 
+    updatedTitle, 
+    updatedImageUrl, 
+    updatedPrice, 
+    updatedDesc
+  )
+  updatedProduct.save()
+  res.redirect('/admin/products')
 }
 
 exports.getProducts = (req, res, next) => {
@@ -22,4 +62,10 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     })
   })
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.deleteById(prodId)
+  res.redirect('/admin/products')
 }
--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,13 @@ router.get('/', shopControllers.getIndex)
 
 router.get('/products', shopControllers.getProducts)
 
+router.get('/products/:productId', shopControllers.getProduct)
+
 router.get('/cart', shopControllers.getCart)
+
+router.post('/cart', shopControllers.postCart)
+
+router.post('/cart-delete-item', shopControllers.postCartDeleteProduct)
 
 router.get('/orders', shopControllers.getOrders)
 
